87e9f86049f277bdf5260f4d5f3d19d844e4755d
yovnsh <yovnsh@gmail.com>
2023-12-18 17:18:50 +0100

    szyfr cezara

---
+++ szyfr cezara/cezar/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wybierz opcję:");
+        Console.WriteLine("1. Szyfrowanie");
+        Console.WriteLine("2. Deszyfrowanie");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine("Podaj tekst do zaszyfrowania:");
+                string tekst_jawny = Console.ReadLine();
+                Console.WriteLine("Podaj przesunięcie:");
+                int skok = int.Parse(Console.ReadLine());
+
+                if( skok < 1 || skok > 34)
+                {
+                    Console.WriteLine("Przesunięcie może występować tylko w przedziale od 1 do 34.");
+                    return;
+                }
+
+                string losoweznakigenerator = Szyfr(tekst_jawny, skok);
+                Console.WriteLine("Zaszyfrowany tekst: " + losoweznakigenerator);
+                break;
+
+            case "2":
+                Console.WriteLine("Podaj tekst do odszyfrowania:");
+                string tekst_zaszyfrowany = Console.ReadLine();
+                Console.WriteLine("Podaj przesunięcie:");
+                int skok_wsteczny = int.Parse(Console.ReadLine());
+
+                if (skok_wsteczny < 1 || skok_wsteczny > 34)
+                {
+                    Console.WriteLine("Przesunięcie może występować tylko w przedziale od 1 do 34.");
+                    return;
+                }
+
+                string nielosoweznakigenerator = Deszyfr(tekst_zaszyfrowany, skok_wsteczny);
+                Console.WriteLine("Odszyfrowany tekst: " + nielosoweznakigenerator);
+                break;
+
+            default:
+                Console.WriteLine("Nieznana opcja.");
+                break;
+        }
+    }
+    const string alfabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ";
+    static string Szyfr(string tekst_jawny, int skok)
+    {
+        char[] syf = tekst_jawny.ToUpper().Trim().ToCharArray();
+
+        for (int i = 0; i < syf.Length; i++)
+        {
+            char tosiedzieje = syf[i];
+            int index = alfabet.IndexOf(tosiedzieje);
+
+            if (index != -1)
+            {
+                int newIndex = (index + skok) % alfabet.Length;
+                syf[i] = alfabet[newIndex];
+            }
+        }
+        return new string(syf);
+    }
+
+    static string Deszyfr(string tekst_zaszyfrowany, int skok_wsteczny)
+    {
+        return Szyfr(tekst_zaszyfrowany, alfabet.Length -skok_wsteczny);
+    }
+}
---
+++ szyfr cezara/mogenie/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+
+namespace CezarCipherWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string plaintext = InputTextBox.Text;
+            if (string.IsNullOrEmpty(plaintext) || string.IsNullOrEmpty(ShiftTextBox.Text))
+            {
+                MessageBox.Show("Należy wpisać tekst oraz przesunięcie.");
+                return;
+            }
+            int shift = int.Parse(ShiftTextBox.Text);
+
+            if (shift < 1 || shift > 34)
+            {
+                MessageBox.Show("Przesunięcie może występować tylko w przedziale od 1 do 34.");
+                return;
+            }
+
+            string encryptedText = Encrypt(plaintext, shift);
+            OutputTextBox.Text = encryptedText;
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string encryptedText = InputTextBox.Text;
+            if (string.IsNullOrEmpty(InputTextBox.Text) || string.IsNullOrEmpty(ShiftTextBox.Text))
+            {
+                MessageBox.Show("Należy wpisać tekst oraz przesunięcie.");
+                return;
+            }
+
+            int shift = int.Parse(ShiftTextBox.Text);
+
+            if (shift < 1 || shift > 34)
+            {
+                MessageBox.Show("Przesunięcie może występować tylko w przedziale od 1 do 34.");
+                return;
+            }
+
+            string decryptedText = Decrypt(encryptedText, shift);
+            OutputTextBox.Text = decryptedText;
+        }
+
+        const string alfabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ";
+
+        private string Encrypt(string plaintext, int shift)
+        {
+            char[] chars = plaintext.ToUpper().Trim().ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                char currentChar = chars[i];
+                int index = alfabet.IndexOf(currentChar);
+
+                if (index != -1)
+                {
+                    int newIndex = (index + shift) % alfabet.Length;
+                    chars[i] = alfabet[newIndex];
+                }
+                else
+                {
+                    MessageBox.Show("Tekst może zawierać tylko znaki znajdujące się w polskim alfabecie.");
+                    return " ";
+                }
+            }
+
+            return new string(chars);
+        }
+
+        private string Decrypt(string encryptedText, int shift)
+        {
+            return Encrypt(encryptedText, alfabet.Length - shift);
+        }
+    }
+}
