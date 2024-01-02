96048a075a9223d355b7f7e2c659477fd6eea90c
yovnsh <yovnsh@gmail.com>
2024-01-02 20:28:00 +0100

    szyfr polibiusza

---
+++ polibiusz/polibiusz_wpf/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace polibiusz_wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string plaintext = InputTextBox.Text;
+            if (string.IsNullOrEmpty(plaintext))
+            {
+                MessageBox.Show("Należy wpisać tekst");
+                return;
+            }
+
+            string encryptedText = PolibiuszSzyfr.Zaszyfruj(plaintext);
+            OutputTextBox.Text = encryptedText;
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string encryptedText = InputTextBox.Text;
+            if (string.IsNullOrEmpty(InputTextBox.Text))
+            {
+                MessageBox.Show("Należy wpisać tekst");
+                return;
+            }
+
+            string decryptedText = PolibiuszSzyfr.Odszyfruj(encryptedText);
+            OutputTextBox.Text = decryptedText;
+        }
+    }
+}
---
+++ polibiusz/polibiusz_wpf/PolibiuszSzyfr.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polibiusz_wpf
+{
+
+    public class PolibiuszSzyfr
+    {
+        static char[,] tabela = new char[,]
+        {
+            {'F', 'Ę', 'O', 'Q', 'Ź', 'Z', 'L'},
+            {'Ą', 'J', 'Ń', 'H', 'C', 'N', 'U'},
+            {'G', 'B', 'A', 'Ż', 'S', 'V', 'W'},
+            {'D', 'E', 'K', 'P', 'T', 'I', 'M'},
+            {'Ł', 'Ć', 'R', 'Ó', 'Ś', 'X', 'Y'}
+        };
+
+        public static string Zaszyfruj(string tekst)
+        {
+            tekst = tekst.ToUpper();
+            string zaszyfrowanyTekst = "";
+
+            foreach (char znak in tekst)
+            {
+                if (char.IsLetter(znak))
+                {
+                    for (int i = 0; i < tabela.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < tabela.GetLength(1); j++)
+                        {
+                            if (tabela[i, j] == znak)
+                            {
+                                zaszyfrowanyTekst += (i + 1).ToString() + (j + 1).ToString() + " ";
+                            }
+                        }
+                    }
+                }
+            }
+
+            return zaszyfrowanyTekst.Trim();
+        }
+
+        public static string Odszyfruj(string zaszyfrowanyTekst)
+        {
+            string odszyfrowanyTekst = "";
+            string[] paryLiczb = zaszyfrowanyTekst.Split(' ');
+
+            foreach (string para in paryLiczb)
+            {
+                int i = int.Parse(para[0].ToString()) - 1;
+                int j = int.Parse(para[1].ToString()) - 1;
+                odszyfrowanyTekst += tabela[i, j].ToString();
+            }
+
+            return odszyfrowanyTekst;
+        }
+
+    }
+}
